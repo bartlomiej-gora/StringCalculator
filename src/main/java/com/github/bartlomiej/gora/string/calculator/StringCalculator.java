5fdf423434b39edbb6420a2dd34b3f8dd51bd2e4
bartlomiej-gora <bartlomiej.gora@gmail.com>
2019-01-01 11:35:12 +0100

    Podstawowe klasy

---
+++ src/main/java/com/github/bartlomiej/gora/string/calculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package com.github.bartlomiej.gora.string.calculator;
+
+class StringCalculator {
+
+    public static int add(final String numbers){
+        return 0;
+    }
+}
---
+++ src/test/java/com/github/bartlomiej/gora/string/calculator/StringCalculatorTest.java
@@ -0,0 +1,7 @@
+package com.github.bartlomiej.gora.string.calculator;
+
+class StringCalculatorTest {
+
+    //@Test
+    //TODO: dodja swój pierwszy test
+}
